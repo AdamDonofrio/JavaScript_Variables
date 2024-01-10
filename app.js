60d762888a258ce9282879e094fde6e62d603128
unknown <donofrio.8@wright.edu>
2024-01-10 14:08:07 -0500

    first commit

--- app.js
+++ app.js
@@ -5,6 +5,41 @@ console.log(
 
 // Exercise 1
 
+const firstname = "name";
+let lastname = "last Name";
+var age = 88;
+
 // Exercise 2
 
+lastname = "Donofrio";
+age = "30";
+
 // Exercise 3
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} came out in ${createdYear}`);
+
+// Exercise 4
+
+let product = "lamp";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`${product} is $${price} and in size ${selectedSize} with ${stock} in stock`);
+
+// Exercise 5
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${title} is ${pageCount} pages long, by ${author}`);
